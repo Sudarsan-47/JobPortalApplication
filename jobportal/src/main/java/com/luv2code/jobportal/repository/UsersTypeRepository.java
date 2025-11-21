837af239e124846701a7949b08175bcf3ea9d64f
Sudarsan-47 <mssudarsan4478@gmail.com>
2025-11-21 22:01:55 +0530

    Final job portal code commit

---
+++ jobportal/src/main/java/com/luv2code/jobportal/repository/UsersTypeRepository.java
@@ -0,0 +1,10 @@
+package com.luv2code.jobportal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.jobportal.entity.UsersType;
+
+public interface UsersTypeRepository extends JpaRepository<UsersType, Integer>{
+	
+
+}
---
+++ jobportal/src/main/java/com/luv2code/jobportal/services/UsersTypeService.java
@@ -0,0 +1,26 @@
+package com.luv2code.jobportal.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luv2code.jobportal.entity.UsersType;
+import com.luv2code.jobportal.repository.UsersTypeRepository;
+
+@Service
+public class UsersTypeService {
+	private final UsersTypeRepository usersTypeRepository;
+
+	@Autowired
+	public UsersTypeService(UsersTypeRepository usersTypeRepository) {
+		this.usersTypeRepository = usersTypeRepository;
+	}
+	
+	public List<UsersType> getAll(){
+		return usersTypeRepository.findAll();
+	}
+	
+	
+
+}
